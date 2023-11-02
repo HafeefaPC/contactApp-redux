e6cb284dbb55eca7f3a349d80632151d5166e4f6
HafeefaPC <hafeefapc2003@gmail.com>
2023-11-02 23:00:42 +0530

    error encoundered

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
 
 import configureStore from './store/configureStore';
 
