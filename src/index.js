807340eef4d8c4176bb7fd59cbf413545f3c15bb
HafeefaPC <hafeefapc2003@gmail.com>
2023-11-02 22:35:56 +0530

    code part completed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
     <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker();  
